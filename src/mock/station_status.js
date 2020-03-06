f1138fe3e041a605ef79af2cbc061576ed4d73c3
Helge Fredheim <helge.fredheim@bekk.no>
2020-03-06 18:51:25 +0100

    Legger til mock-data til bruk i tester

---
+++ src/mock/station_status.js
@@ -0,0 +1,46 @@
+const stationStatus = {
+  last_updated: 1583516575,
+  ttl: 10,
+  data: {
+    stations: [
+      {
+        station_id: "1755",
+        is_installed: 1,
+        is_renting: 1,
+        is_returning: 1,
+        last_reported: 1583516575,
+        num_bikes_available: 0,
+        num_docks_available: 33
+      },
+      {
+        station_id: "1101",
+        is_installed: 1,
+        is_renting: 1,
+        is_returning: 1,
+        last_reported: 1583516575,
+        num_bikes_available: 0,
+        num_docks_available: 24
+      },
+      {
+        station_id: "1023",
+        is_installed: 1,
+        is_renting: 1,
+        is_returning: 1,
+        last_reported: 1583516575,
+        num_bikes_available: 0,
+        num_docks_available: 12
+      },
+      {
+        station_id: "1009",
+        is_installed: 1,
+        is_renting: 1,
+        is_returning: 1,
+        last_reported: 1583516575,
+        num_bikes_available: 0,
+        num_docks_available: 10
+      }
+    ]
+  }
+};
+
+export default stationStatus;
